a4ea376e3ccf4229c8428b8d34c0599d949c2feb
Zhichao Li <b1tank@outlook.com>
2026-07-08 10:42:06 -0700

    Add -v/-vv verbose ASan modes to toyforth run.sh

--- courses/antirez/toyforth/run.sh
+++ courses/antirez/toyforth/run.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
+set -e
 
-cc toyforth.c -Wall -W -O2 && ./a.out program.tf
+verbose=0
+[[ "$1" == "-v" ]] && { verbose=1; shift; }
+[[ "$1" == "-vv" ]] && { verbose=2; shift; }
+
+prog="${1:-program.tf}"
+
+if [[ "$verbose" -ge 1 ]]; then
+	echo "== verbose build (debug + AddressSanitizer) =="
+	cc toyforth.c -g -fsanitize=address -Wall -Wextra -O0 -o a.out
+else
+	cc toyforth.c -Wall -W -O2 -o a.out
+fi
+
+if [[ "$verbose" -ge 2 ]]; then
+	ASAN_OPTIONS=abort_on_error=1:halt_on_error=1 ./a.out "$prog"
+else
+	./a.out "$prog"
+fi
